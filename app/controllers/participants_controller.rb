8fd3ea206ec78aa4664241db050f7c6195679024
Sandro Padin <sandropadin@gmail.com>
2011-05-28 16:06:46 -0500

    Adding create action to participants controller.

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -5,4 +5,8 @@ class ParticipantsController < ApplicationController
         format.json  { render :json => @participants }
     end
   end
+  def create
+    Participants.new(:conference_id => params[:conference_id], :user_id => params[:user_id]).save
+    render :text => "ok"
+  end
 end
