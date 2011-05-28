cfdc32f742a39daba033c47d3cea688a718fff61
Sandro Padin <sandropadin@gmail.com>
2011-05-28 14:23:21 -0500

    Logs in JSON format.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,9 +1,16 @@
 class LogsController < ApplicationController
+  respond_to :json
+  
   def index
+    @messages = Logs.find(:all)
+    respond_to do |format|
+        format.html # index.html.erb
+        format.json  { render :json => @messages }
+    end
   end
 
   def create
-    Logs.new(:message => "TEST").save
+    Logs.new(:message => params[:message]).save
+    render :text => "ok"
   end
-
 end
