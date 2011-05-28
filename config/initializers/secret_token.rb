3e98747d43c02dab907fcac68aec4fa5e83928fd
Sandro Padin <sandropadin@gmail.com>
2011-05-28 11:46:47 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Translatio::Application.config.secret_token = '636bbf9b31fc4a8c8cfad75dafbf4927c7ff879bc0594301fa104106729992a1d342c5d921783958ff83efa6864084144ef123344876cafd72b24e0e9ed7e937'
