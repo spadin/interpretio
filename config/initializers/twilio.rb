ec0a8690e7eb30d4198ec064be8d9afb5c46d384
Matt Gornick <mattgornick@gmail.com>
2011-05-28 17:58:50 -0500

    twillio

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,8 +1,17 @@
 require "yaml"
-config_hash = YAML::load_file("#{RAILS_ROOT}/config/twilio.yml")
 
-# This should be in an initializer or similar
-Twilio::Config.setup do
-  account_sid  config_hash['account_sid']
-  auth_token   config_hash['auth_token']
+if RAILS_ENV == "production"
+  Twilio::Config.setup do
+    account_sid  ENV['account_sid']
+    auth_token   ENV['auth_token']
+  end
+else
+
+  config_hash = YAML::load_file("#{RAILS_ROOT}/config/twilio.yml")
+
+  # This should be in an initializer or similar
+  Twilio::Config.setup do
+    account_sid  config_hash['account_sid']
+    auth_token   config_hash['auth_token']
+  end
 end
