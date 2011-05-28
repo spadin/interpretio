521928994a3d7aec7c9eee15d6c1014a53eec33b
Sandro Padin <sandropadin@gmail.com>
2011-05-28 16:14:49 -0500

    Allows JSONP callbacks.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,18 +1,8 @@
 class LogsController < ApplicationController
-  respond_to :json
-  
-  # def index
-  #   @messages = Logs.find(:all)
-  #   respond_to do |format|
-  #       format.html # index.html.erb
-  #       format.json  { render :json => @messages }
-  #   end
-  # end
-  
   def list
     @messages = Logs.find(:all)
     respond_to do |format|
-        format.json  { render :json => @messages }
+        format.json  { render :json => @messages, :callback => params[:callback] }
     end
   end
 
--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,9 +2,10 @@ class ParticipantsController < ApplicationController
   def list
     @participants = Participants.find(:all)
     respond_to do |format|
-        format.json  { render :json => @participants }
+        format.json  { render :json => @participants, :callback => params[:callback] }
     end
   end
+  
   def create
     Participants.new(:conference_id => params[:conference_id], :user_id => params[:user_id]).save
     render :text => "ok"
