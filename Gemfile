bf031478a55cc890972882051ee3ebdf61f0f995
Agam Patel <agamrp@gmail.com>
2011-05-28 15:37:47 -0500

    adding gravatar, stopped messing around with formstastic for the time being

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.7'
 gem 'sqlite3'
-gem 'formtastic'
+gem 'gravtastic'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  include Gravtastic
+  gravtastic
+  
   has_and_belongs_to_many :languages
   accepts_nested_attributes_for :languages
 end
